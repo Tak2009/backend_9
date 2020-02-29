432e3414eb3d3a820016ff6276ba4ac668b345f2
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-02-29 23:24:46 +0000

    readme

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
-# README
+## Requirement
+Using the language of your choice please 
+    i. build your own API which calls the API at https://bpdts-test-app.herokuapp.com/, 
+    ii. and returns 
+        a) people who are listed as either living in London,
+        b) or whose current coordinates are within 50 miles of London. 
 
-Note:
+
+###################################################################################################################################################################################
+
+## Brief Explanation
+
+This is an addtional project to demopnstrate my skills to achive the same objectives. In this project, I built a backend-frontend application.
+For the backend, I used again Ruby on Rails. For the frontend, I used JavaScript.
+In this additional project, the frontend handles all the process to return ii - a) and ii -b). My backend just stores the data available
+at https://bpdts-test-app.herokuapp.com/ and hence it's just a "pass-through" API for my frontend.
+
+My frontend: https://github.com/Tak2009/frontend_9
+My backend: https://github.com/Tak2009/backend_9
+
+1 - How to use my bnackend API and frontend:
+
+    a. $ rails s to launch the web server (https://github.com/Tak2009/backend_9) then,
+    b. once the server has launched, open http://localhost:3000/london_users and http://localhost:3000/users to check if the API is running properly
+    c. $ open index.html in my frontend (https://github.com/Tak2009/frontend_9)
+
+2 - You can find the logic for ii-a) and ii-b) in seed.rb as I applied it to the population of the dababase so that the controllers can render the data faster with simpler logic
+
+4 - For testing purposes, I used dummy data (3 records in seed.rd. They are all commented out) to check the accuracy against Google Maps. 
+Currently the database includes only the data available at https://bpdts-test-app.herokuapp.com/. If you would like to test the logic and also would like to use the dummy data,
+please drop the databases and schema.rb then: 
+
+    a. $ rails db:migrate then,
+    b. $ rails db:seed.
+
+5 - Integration testing has been performed with RSpec to confirm that GET requests work properly.
 
 Gem "rack-cors" activated then bundle install
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,12 @@ require 'json'
 
 uri_all= URI.parse('https://bpdts-test-app.herokuapp.com/users') # all user
 json_all = Net::HTTP.get(uri_all)
-all_users = JSON.parse(json_all) #返ってきたjsonデータをrubyの配列に変換するためのライン
+all_users = JSON.parse(json_all)
 
 uri_london = URI.parse('https://bpdts-test-app.herokuapp.com/city/London/users') # london user
-json_london = Net::HTTP.get(uri_london) # with NET::HTTP, call the API
-london_users = JSON.parse(json_london) #返ってきたjsonデータをrubyの配列に変換するためのライン
+json_london = Net::HTTP.get(uri_london) 
+london_users = JSON.parse(json_london)
 
-# byebug
 
 all_users.each do |user| User.create(
     user_id: user['id'],
@@ -24,9 +23,10 @@ all_users.each do |user| User.create(
 end
 
 # dummy data for checking the distance calc function in frontend agisnt google map
-u1001 = User.create(:user_id => 1001, :first_name => "Huddersfield", :last_name => "Station", :latitude => 53.648557, :longitude => -1.784449) # 163.48 mi, same as google map result. this should not be included in the 50 mi group
-u1002 = User.create(:user_id => 1002, :first_name => "Great Portland", :last_name => "Station", :latitude => 51.524262, :longitude => -0.143772) # 1.48 mi, same as google map result. this should be included in the 50 mi group
-u1003 = User.create(:user_id => 1003, :first_name => "East Ilsley", :last_name => "", :latitude => 51.533576, :longitude => -1.289984) # 50.41 mi same as google map result. this should not be included in the 50 mi group.
+# u1001 = User.create(:user_id => 1001, :first_name => "Huddersfield", :last_name => "Station", :latitude => 53.648557, :longitude => -1.784449) # 163.48 mi, same as google map result. this should not be included in the 50 mi group
+# u1002 = User.create(:user_id => 1002, :first_name => "Great Portland", :last_name => "Station", :latitude => 51.524262, :longitude => -0.143772) # 1.48 mi, same as google map result. this should be included in the 50 mi group
+# u1003 = User.create(:user_id => 1003, :first_name => "East Ilsley", :last_name => "", :latitude => 51.533576, :longitude => -1.289984) # 50.41 mi same as google map result. this should not be included in the 50 mi group.
+
 
 london_users.each do |london_user| Londonuser.create(
     user_id: london_user['id'],
