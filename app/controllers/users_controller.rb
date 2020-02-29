cdec1a8771189b922235a75d6e3d617034406a4e
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-02-29 19:28:20 +0000

    route changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,8 @@
 class UsersController < ApplicationController
 
     def index
-        @users = User.all
-        render json: @users, except: [:created_at, :updated_at]
+        users = User.all
+        render json: users, except: [:created_at, :updated_at]
     end
-
-    def show
-        render json: @user, except: [:created_at, :updated_at]
-    end
-
 
 end
